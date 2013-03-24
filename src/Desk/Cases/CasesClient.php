8bec83c9d255d6b192a2ff69667b491d7b0ce5a9
Brad Feehan <git@bradfeehan.com>
2013-03-24 19:57:43 +1100

    Add getCaseStatus() to convert between IDs/names

--- src/Desk/Cases/CasesClient.php
+++ src/Desk/Cases/CasesClient.php
@@ -3,10 +3,86 @@
 namespace Desk\Cases;
 
 use Desk\AbstractClient;
+use InvalidArgumentException;
 
 /**
  * Client for Desk case API
  */
 class CasesClient extends AbstractClient
 {
+
+    /**
+     * Represents a case status' ID
+     *
+     * @var string
+     */
+    const CASE_STATUS_TYPE_ID = 'id';
+
+    /**
+     * Represents a case status' descriptive name
+     *
+     * @var string
+     */
+    const CASE_STATUS_TYPE_NAME = 'name';
+
+    /**
+     * Maps case status IDs to names
+     *
+     * @var array
+     */
+    private static $caseStatusIdToName = array(
+        10         => 'new',
+        30         => 'open',
+        50         => 'pending',
+        70         => 'resolved',
+        90         => 'closed',
+    );
+
+    /**
+     * Maps case status names to IDs
+     *
+     * @var array
+     */
+    private static $caseStatusNameToId = array(
+        'new'      => 10,
+        'open'     => 30,
+        'pending'  => 50,
+        'resolved' => 70,
+        'closed'   => 90,
+    );
+
+    /**
+     * Converts between representations of a case's status
+     *
+     * This function converts a case status between its numerical ID
+     * (used internally by Desk.com) and the descriptive name (also
+     * used by Desk.com at times).
+     *
+     * @param mixed  $status The status to convert
+     * @param string $type   The type of status to convert to (either
+     *                       CASE_STATUS_TYPE_ID or CASE_STATUS_TYPE_NAME)
+     *
+     * @return mixed A representation of the status in $status; an
+     *               integer if $type === CASE_STATUS_TYPE_ID, or a
+     *               string if $type === CASE_STATUS_TYPE_NAME
+     */
+    public static function getCaseStatus($status, $type = self::CASE_STATUS_TYPE_ID)
+    {
+        switch ($type) {
+            case self::CASE_STATUS_TYPE_ID:
+                if (isset(self::$caseStatusNameToId[$status])) {
+                    return self::$caseStatusNameToId[$status];
+                }
+                break;
+            case self::CASE_STATUS_TYPE_NAME:
+                if (isset(self::$caseStatusIdToName[$status])) {
+                    return self::$caseStatusIdToName[$status];
+                }
+                break;
+            default:
+                throw new InvalidArgumentException("Invalid case status type '$type'");
+        }
+
+        throw new InvalidArgumentException("Invalid case status $type '$status'");
+    }
 }
--- tests/Desk/Tests/Cases/CasesClientTest.php
+++ tests/Desk/Tests/Cases/CasesClientTest.php
@@ -27,4 +27,44 @@ class CasesClientTest extends \Desk\Testing\UnitTestCase
         $filename = CasesClient::getServiceDescriptionFilename();
         $this->assertStringEndsWith('/src/Desk/Cases/client.json', $filename);
     }
+
+    /**
+     * @covers Desk\Cases\CasesClient::getCaseStatus
+     * @dataProvider dataGetCaseStatusValid
+     */
+    public function testGetCaseStatusValid($status, $type, $expected)
+    {
+        $result = CasesClient::getCaseStatus($status, $type);
+        $this->assertSame($expected, $result);
+    }
+
+    public function dataGetCaseStatusValid()
+    {
+        return array(
+            array(10, CasesClient::CASE_STATUS_TYPE_NAME, 'new'),
+            array(30, CasesClient::CASE_STATUS_TYPE_NAME, 'open'),
+            array('pending', CasesClient::CASE_STATUS_TYPE_ID, 50),
+            array('resolved', CasesClient::CASE_STATUS_TYPE_ID, 70),
+        );
+    }
+
+    /**
+     * @covers Desk\Cases\CasesClient::getCaseStatus
+     * @dataProvider dataGetCaseStatusInvalid
+     * @expectedException InvalidArgumentException
+     */
+    public function testGetCaseStatusInvalid($status, $type)
+    {
+        CasesClient::getCaseStatus($status, $type);
+    }
+
+    public function dataGetCaseStatusInvalid()
+    {
+        return array(
+            array('opne', CasesClient::CASE_STATUS_TYPE_ID), // typo
+            array(20, CasesClient::CASE_STATUS_TYPE_NAME), // invalid
+            array(10, CasesClient::CASE_STATUS_TYPE_ID), // wrong type
+            array(10, 'foobar'), // wrong type
+        );
+    }
 }

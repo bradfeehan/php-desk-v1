22c8ee415d5ed2891d2a549297fd9a47e13e2b23
Brad Feehan <git@bradfeehan.com>
2013-03-25 17:12:16 +1100

    Add UpdateCase command to modify cases

---
+++ tests/Desk/Tests/Cases/Command/UpdateCaseCommandTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Desk\Tests\Cases\Command;
+
+class UpdateCaseCommandTest extends \Desk\Testing\OperationTestCase
+{
+
+    protected $client = 'mock.cases';
+
+    protected $operation = 'UpdateCase';
+
+
+    public function dataParameterValid()
+    {
+        return array(
+            array(
+                array(
+                    'id' => 22,
+                    'subject' => 'New Subject',
+                    'labels' => array('foo', 'bar'),
+                ),
+            ),
+            array(
+                array(
+                    'id' => 43,
+                    'status' => 'pending',
+                ),
+                '/case_status_type_id=50/',
+            ),
+        );
+    }
+
+    public function dataParameterInvalid()
+    {
+        return array(
+            array(array(
+                'subject' => 'Missing ID',
+            )),
+            array(array(
+                'id' => 34,
+                'status' => 'not_a_valid_status',
+            )),
+        );
+    }
+}

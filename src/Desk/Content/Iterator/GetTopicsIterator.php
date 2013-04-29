32d5883b1651e7a25a796b8ea409286567d74ed3
Brad Feehan <git@bradfeehan.com>
2013-04-29 16:04:33 +1000

    Add iterator for GetTopics command

---
+++ src/Desk/Content/Iterator/GetTopicsIterator.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Desk\Content\Iterator;
+
+use Guzzle\Service\Resource\ResourceIterator;
+
+/**
+ * Iterator for the GetTopics command
+ */
+class GetTopicsIterator extends ResourceIterator
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function sendRequest()
+    {
+        if ($this->nextToken) {
+            $this->command->set('page', $this->nextToken);
+        }
+
+        $topics = $this->command->execute();
+
+        // Update nextToken, if there are any results
+        if (count($topics)) {
+            $this->nextToken = ((int) $this->nextToken) + 1;
+            return $topics;
+        } else {
+            $this->nextToken = null;
+        }
+
+        return null;
+    }
+}
